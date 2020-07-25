0652c939e3d3d211547c6777bde41f4cab28d266
brezavad <53546576+brezavad@users.noreply.github.com>
2020-07-25 16:50:06 -0500

    Added support for menu system
    Added support for menu system

--- Scenes/GameScreen.py
+++ Scenes/GameScreen.py
@@ -1,5 +1,5 @@
 from Bases.SceneBase import SceneBase
-from Objects.GameScreenObjects import BackgroundImage, GameScreenMessage
+from Objects.GameScreenObjects import BackgroundImage, GameScreenMessage, GameScreenStatusMenu
 from Objects.BattleshipBoard import BattleshipBoard
 from Objects.GameScreenObjects import GameSceneManager
 
@@ -11,14 +11,16 @@ class GameScreen(SceneBase):
         :param il: the imageloader
         """
         SceneBase.__init__(self, il)
+        self.status_menu = GameScreenStatusMenu(self.IL)
+        self.player_board = BattleshipBoard(self.IL, 100, 200)
+        self.enemy_board = BattleshipBoard(self.IL, 550, 200)
+
         self.OH.new_object(BackgroundImage(self.IL))
-        self.OH.new_object(GameScreenMessage(self.IL))
-        self.player_board = BattleshipBoard(self.IL, 100, 150)
+        # self.OH.new_object(GameScreenMessage(self.IL))
+        self.OH.new_object(self.status_menu)
         self.OH.new_object(self.player_board)
-        self.enemy_board = BattleshipBoard(self.IL, 550, 150)
         self.OH.new_object(self.enemy_board)
-        self.OH.new_object(GameSceneManager(self.IL, self.player_board,
-                           self.enemy_board))
+        self.OH.new_object(GameSceneManager(self.IL, self.status_menu, self.player_board, self.enemy_board))
 
     def update(self):
         SceneBase.update(self)
