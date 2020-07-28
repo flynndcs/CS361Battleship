dadb1406b2e4ba5984203f3c2d5e8c8a5ac3b13c
brezavad <53546576+brezavad@users.noreply.github.com>
2020-07-28 15:52:28 -0500

    Fixed bugs
    Fixed the following bugs:
    
    1) index out of range
    2) mouse pointer not selecting the right squares

--- Objects/BattleshipBoard.py
+++ Objects/BattleshipBoard.py
@@ -151,115 +151,115 @@ class BattleshipBoard(BaseObject):
         self.image.blit(highlightRect, boardPosition)
 
         
-    def _update_ship_count_number(self, ship_name, t = 0):
-        '''
-        Uses the ship_count_tracker class dictionary to track the number of positions
-        each ship has taken up on the board. 
-        '''
+    # def _update_ship_count_number(self, ship_name, t = 0):
+    #     '''
+    #     Uses the ship_count_tracker class dictionary to track the number of positions
+    #     each ship has taken up on the board. 
+    #     '''
 
-        if (t == 0):
-            if ship_name in self.ship_count_tracker:
-                self.ship_count_tracker[ship_name] += 1
-            else:
-                self.ship_count_tracker[ship_name] = 1
-        else:
-            self.ship_count_tracker[ship_name] -= 1
+    #     if (t == 0):
+    #         if ship_name in self.ship_count_tracker:
+    #             self.ship_count_tracker[ship_name] += 1
+    #         else:
+    #             self.ship_count_tracker[ship_name] = 1
+    #     else:
+    #         self.ship_count_tracker[ship_name] -= 1
 
-    def _is_ship_sunk(self, ship_name):
-        '''
-        Determines if the ship that was hit has been sunk
-        '''
+    # def _is_ship_sunk(self, ship_name):
+    #     '''
+    #     Determines if the ship that was hit has been sunk
+    #     '''
 
-        if (self.ship_count_tracker[ship_name] == 0):
-            return True
+    #     if (self.ship_count_tracker[ship_name] == 0):
+    #         return True
 
-        return False
+    #     return False
 
-    def _extract_location(self, location):
-        '''
-        Return the row and column location as an array with 2 elements
+    # def _extract_location(self, location):
+    #     '''
+    #     Return the row and column location as an array with 2 elements
 
-        Precondition: location must be in the format "a-b" where a is the row
-                      number and b is the column number
-        '''
+    #     Precondition: location must be in the format "a-b" where a is the row
+    #                   number and b is the column number
+    #     '''
 
-        return location.split("-")
+    #     return location.split("-")
 
-    def _update_location_to_used(self, row, column):
-        '''
-        Updates the gameboard location to "used" to signify that location has
-        already been guessed
-        '''
+    # def _update_location_to_used(self, row, column):
+    #     '''
+    #     Updates the gameboard location to "used" to signify that location has
+    #     already been guessed
+    #     '''
 
-        self.gameboard[row][column] = "used"
+    #     self.gameboard[row][column] = "used"
 
-    def _get_gameboard_info(self, row, column):
-        '''
-        Returns string of the gameboard that is located at the row and column
-        position passed into function
-        '''
+    # def _get_gameboard_info(self, row, column):
+    #     '''
+    #     Returns string of the gameboard that is located at the row and column
+    #     position passed into function
+    #     '''
 
-        return self.gameboard[row][column]
+    #     return self.gameboard[row][column]
 
-    def _gameboard_hit(self, row, column, ship_name):
-        '''
-        Updates the gameboard and ship_count_tracker to account for a hit on the board.
-        '''
+    # def _gameboard_hit(self, row, column, ship_name):
+    #     '''
+    #     Updates the gameboard and ship_count_tracker to account for a hit on the board.
+    #     '''
         
-        self._update_ship_count_number(ship_name, 1)
-        self.total_ship_positions -= 1
+    #     self._update_ship_count_number(ship_name, 1)
+    #     self.total_ship_positions -= 1
 
-    def add_ship(self, ship_name, ship_array):
-        '''
-        Add a ship to the game board. 
+    # def add_ship(self, ship_name, ship_array):
+    #     '''
+    #     Add a ship to the game board. 
 
-        Precondition: funtion must be passed a ship name
+    #     Precondition: funtion must be passed a ship name
 
-        Precondition: function must be passed an array of locations. Each element
-                      of the location array must be structured "a-b" where a is the
-                      row number and b is the column number
-        '''
-        for location in ship_array:
-            row, column = self._extract_location(location)
+    #     Precondition: function must be passed an array of locations. Each element
+    #                   of the location array must be structured "a-b" where a is the
+    #                   row number and b is the column number
+    #     '''
+    #     for location in ship_array:
+    #         row, column = self._extract_location(location)
 
-            self.total_ship_positions += 1
-            self.gameboard[row][column] = ship_name
-            self._update_ship_count_number(ship_name, 0)
+    #         self.total_ship_positions += 1
+    #         self.gameboard[row][column] = ship_name
+    #         self._update_ship_count_number(ship_name, 0)
 
-    def all_ships_sunk_check(self):
-        '''
-        Determine if all ships have been sunk on a gameboard. Return true if 
-        all ships are sunk. Otherwise return false
-        '''
+    # def all_ships_sunk_check(self):
+    #     '''
+    #     Determine if all ships have been sunk on a gameboard. Return true if 
+    #     all ships are sunk. Otherwise return false
+    #     '''
 
-        if (self.total_ship_positions == 0):
-            return True
+    #     if (self.total_ship_positions == 0):
+    #         return True
 
-        return False
+    #     return False
 
-    def make_guess(self, location):
-        '''
-        Allows for the user or AI to make a guess on the board.
+    # def make_guess(self, location):
+    #     '''
+    #     Allows for the user or AI to make a guess on the board.
 
-        Precondition: location must be in the format "a-b" where a is the row
-                      number and b is the column number
+    #     Precondition: location must be in the format "a-b" where a is the row
+    #                   number and b is the column number
 
-        Return types:
-            0 = Guess was a miss
-            1 = Guess was a hit
-            2 = Location has already been guessed
-        '''
+    #     Return types:
+    #         0 = Guess was a miss
+    #         1 = Guess was a hit
+    #         2 = Location has already been guessed
+    #     '''
 
-        row, column = self._extract_location(location)
-        location_info = self._get_gameboard_info(row, column)
+    #     row, column = self._extract_location(location)
+    #     location_info = self._get_gameboard_info(row, column)
 
-        if (location_info == "used"):
-            return 2
+    #     if (location_info == "used"):
+    #         return 2
         
-        self._update_location_to_used(row, column)
+    #     self._update_location_to_used(row, column)
 
-        if (location_info == "0"):
-            return 0
-        else:
-            self._gameboard_hit(row, column, location_info)
-            return 1
+    #     if (location_info == "0"):
+    #         return 0
+    #     else:
+    #         self._gameboard_hit(row, column, location_info)
+    #         return 1
